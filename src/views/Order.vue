6626c6e0392e1e3c202790b062a8f722d10e2d8d
Tyson Koger <tkoger@cpg.org>
2022-11-19 07:11:44 -0700

    Update lables

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -10,7 +10,7 @@
         >
             <template v-slot:top>
                 <v-toolbar flat color="white">
-                    <v-toolbar-title>Orders</v-toolbar-title>
+                    <v-toolbar-title>Active Orders</v-toolbar-title>
                     <v-divider class="mx-4" inset vertical/>
                     <v-spacer/>
 
@@ -62,7 +62,7 @@
                         <template v-slot:activator="{ on }">
                             <v-icon small @click="cancelOrder(group)" v-on="on">mdi-delete</v-icon>
                         </template>
-                        <span>Delete Order</span>
+                        <span>Cancel Order</span>
                     </v-tooltip>
                 </td>
             </template>
